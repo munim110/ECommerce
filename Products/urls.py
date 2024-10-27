5e42f0809174f37f7f1775affbc630c0c0dc9dfc
munim110 <munim987654321@gmail.com>
2024-10-27 23:25:40 +0600

    Added product details

--- Products/urls.py
+++ Products/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('products', all_products, name='products'),
+    path('product/<int:id>', product_detail, name='product_detail'),
 ]
--- Products/views.py
+++ Products/views.py
@@ -9,3 +9,13 @@ def all_products(request):
         "products": all_products
     }
     return render(request, 'all_products.html', context)
+
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        product = None
+    context = {
+        "product": product
+    }
+    return render(request, 'product_details.html', context)
